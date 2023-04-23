028306af5047a9a7a0f27ce472e287f78db96a2f
Kevin Baquero <baquerochavarro@gmail.com>
2023-04-23 11:20:49 -0500

    Initial configuration
    Project structure

--- CampusVirtual.API/Program.cs
+++ CampusVirtual.API/Program.cs
@@ -1,3 +1,8 @@
+using AutoMapper.Data;
+using CampusVirtual.API.AutoMapper;
+using CampusVirtual.Infrastructure.SQLAdapter;
+using CampusVirtual.Infrastructure.SQLAdapter.Gateway;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,14 +11,22 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAutoMapper(config => config.AddDataReaderMapping(), typeof(ConfigurationProfile));
+
+
+
+builder.Services.AddTransient<IDbConnectionBuilder>(e =>
+{
+    return new DbConnectionBuilder(builder.Configuration.GetConnectionString("urlConnectionSQL"));
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-	app.UseSwagger();
-	app.UseSwaggerUI();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
---
+++ Users.Infrastructure/MongoAdapter/Interfaces/IContext.cs
@@ -0,0 +1,10 @@
+﻿using MongoDB.Driver;
+//using Users.Infrastructure.Entities;
+
+namespace Users.Infrastructure.MongoAdapter.Interfaces
+{
+    public interface IContext
+    {
+        //public IMongoCollection<UserMongo> Users { get; }
+    }
+}
