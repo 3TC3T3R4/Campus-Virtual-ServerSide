08593d496003398badcc144887d9c48abc03bd82
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-23 15:28:49 -0500

    Created Commands

---
+++ CampusVirtual.Domain/Commands/InsertNewLearningPath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.Domain.Commands
+{
+    public class InsertNewLearningPath
+    {
+      
+        public string CoachID { get; private set; }
+        public string Title { get; private set; }
+        public string Description { get; private set; }
+        public decimal Duration { get; private set; }
+       // public int StatePath { get; private set; }
+
+
+
+
+
+    }
+}
