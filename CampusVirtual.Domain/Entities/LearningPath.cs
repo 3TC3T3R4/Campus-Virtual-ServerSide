1b5172ee64310aa315470cbba222756642ac13e5
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-23 15:25:03 -0500

    Created Entity LearningPath

---
+++ CampusVirtual.Domain/Entities/LearningPath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.Domain.Entities
+{
+    public class LearningPath
+    {
+        public Guid PathID { get;  private set; }
+        public string CoachID { get; private set; }
+        public string Title { get;  private set; }
+        public string Description { get;  private set; }
+        public decimal Duration { get;  private set; }
+        public int StatePath { get;  private set; }
+
+
+
+
+
+
+    }
+}
