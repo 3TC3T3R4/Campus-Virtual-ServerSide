95f148b57049b516aa8d5e7fdbd78ed01543fe1e
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-23 15:32:42 -0500

    Created interface UseCase

---
+++ CampusVirtual.UseCases/Gateway/ILearningPathUseCase.cs
@@ -0,0 +1,16 @@
+﻿using CampusVirtual.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.UseCases.Gateway
+{
+    public interface ILearningPathUseCase
+    {
+        Task<List<LearningPath>> GetLearningPathsAsync();
+
+
+    }
+}
