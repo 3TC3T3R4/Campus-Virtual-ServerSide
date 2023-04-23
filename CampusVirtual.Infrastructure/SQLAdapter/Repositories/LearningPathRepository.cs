eebf916018224600ef4c37bf2992e18cab0bafa8
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-23 15:47:42 -0500

    Created first services get List LearningPaths

---
+++ CampusVirtual.Infrastructure/SQLAdapter/Repositories/LearningPathRepository.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using CampusVirtual.Domain.Entities;
+using CampusVirtual.Infrastructure.SQLAdapter.Gateway;
+using CampusVirtual.UseCases.Gateway.Repositories;
+using Dapper;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.Infrastructure.SQLAdapter.Repositories
+{
+    public class LearningPathRepository: ILearningPathRepository
+    {
+        private readonly IDbConnectionBuilder _dbConnectionBuilder;
+        private readonly IMapper _mapper;
+
+        private readonly string _tableNameLearningPaths = "LearningPaths";
+        
+        public LearningPathRepository(IDbConnectionBuilder dbConnectionBuilder, IMapper mapper)
+        {
+            _dbConnectionBuilder = dbConnectionBuilder;
+            _mapper = mapper;
+        }
+
+        public async  Task<List<LearningPath>> GetLearningPathsAsync()
+        {
+            var connection = await _dbConnectionBuilder.CreateConnectionAsync();
+            string sqlQuery = $"SELECT * FROM {_tableNameLearningPaths}";
+            var result = await connection.QueryAsync<LearningPath>(sqlQuery);
+            if
+            (
+                result.IsNullOrEmpty()
+            )
+            {
+                throw new Exception("No LearningPaths found");
+            }
+            connection.Close();
+            return result.ToList();
+        }
+    }
+}
