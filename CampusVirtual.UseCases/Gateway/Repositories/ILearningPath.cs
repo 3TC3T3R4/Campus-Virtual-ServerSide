a7fa2b6cfb9e59ff2b8a2d431987983b5ac0e310
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-23 15:31:20 -0500

    created intarface learningpath

---
+++ CampusVirtual.UseCases/Gateway/Repositories/ILearningPath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampusVirtual.UseCases.Gateway.Repositories
+{
+    public interfaces ILearningPath
+    {
+
+        Task<List<LearningPath>> GetLearningPathsAsync();
+
+
+
+    }
+}
