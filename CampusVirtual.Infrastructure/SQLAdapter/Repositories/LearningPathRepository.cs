525217e5b09058c0762dc348ea3d91239f549f80
ESTEVAN TANGARIFE CORREA <teban1928@gmail.com>
2023-04-24 17:56:50 -0500

    implements Ardalis Guards

--- CampusVirtual.Infrastructure/SQLAdapter/Repositories/LearningPathRepository.cs
+++ CampusVirtual.Infrastructure/SQLAdapter/Repositories/LearningPathRepository.cs
@@ -79,7 +79,8 @@ namespace CampusVirtual.Infrastructure.SQLAdapter.Repositories
         public async Task<List<LearningPath>> GetLearningPathsByCoachAsync(string ID)
         {
 
-
+           Guard.Against.Null(ID, nameof(ID), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(ID, nameof(ID), "Ingresa por favor el id del coach, no puede ser vacio o nulo");
 
 
             var connection = await _dbConnectionBuilder.CreateConnectionAsync();
@@ -103,16 +104,41 @@ namespace CampusVirtual.Infrastructure.SQLAdapter.Repositories
 
         public async Task<InsertNewLearningPath> UpdateLearningPathByIdAsync(string idPath, InsertNewLearningPath path)
         {
+
+            Guard.Against.Null(idPath, nameof(idPath), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(idPath, nameof(idPath), "Ingresa por favor el id  no puede ser vacio o nulo");
+
+            Guard.Against.Null(path.CoachID, nameof(path.CoachID), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(path.CoachID, nameof(path.CoachID), "Ingresa por favor el id  no puede ser vacio o nulo");
+            Guard.Against.Null(path.Title, nameof(path.Title), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(path.Title, nameof(path.Title), "Ingresa por favor el id  no puede ser vacio o nulo");
+
+            Guard.Against.Null(path.Description , nameof(path.Description), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(path.Description, nameof(path.Description), "Ingresa por favor el id  no puede ser vacio o nulo");
+
+            Guard.Against.Null(path.Duration, nameof(path.Duration), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(path.Duration.ToString(), nameof(path.Duration), "Ingresa por favor el id  no puede ser vacio o nulo");
+
+            Guard.Against.Null(path.StatePath, nameof(path.StatePath), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(path.StatePath.ToString(), nameof(path.StatePath), "Ingresa por favor el id  no puede ser vacio o nulo");
+
+
             var connection = await _dbConnectionBuilder.CreateConnectionAsync();
             string sqlQuery = $"UPDATE {_tableNameLearningPaths} SET coachID = @coachID,title = @title,description = @description,duration = @duration,statePath = @statePath WHERE pathID = {idPath}";
             var rows = await connection.ExecuteAsync(sqlQuery, path);
             return path;
+           
+
+
+          
 
 
         }
 
         public async Task<string> DeleteLearningPathByIdAsync(string idPath)
         {
+            Guard.Against.Null(idPath, nameof(idPath), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(idPath, nameof(idPath), "Ingresa por favor el id del coach, no puede ser vacio o nulo");
 
 
             var param = new { delete = 0 };
@@ -127,6 +153,8 @@ namespace CampusVirtual.Infrastructure.SQLAdapter.Repositories
 
         public async Task<LearningPath> GetLearningPathsByIdAsync(string idPath)
         {
+            Guard.Against.Null(idPath, nameof(idPath), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(idPath, nameof(idPath), "Ingresa por favor el id del coach, no puede ser vacio o nulo");
 
 
 
@@ -152,8 +180,11 @@ namespace CampusVirtual.Infrastructure.SQLAdapter.Repositories
 
         public async Task<string> UpdateLearningPathDurationAsync(string idPath, int numberCourses)
         {
+            Guard.Against.Null(idPath, nameof(idPath), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(idPath, nameof(idPath), "Ingresa por favor el id del coach, no puede ser vacio o nulo");
+            Guard.Against.Null(numberCourses, nameof(numberCourses), "Ingresa el campo por favor");
+            Guard.Against.NullOrEmpty(numberCourses.ToString(), nameof(numberCourses), "Ingresa por favor el id del coach, no puede ser vacio o nulo");
 
-          
             var connection = await _dbConnectionBuilder.CreateConnectionAsync();
             string sqlQuery = $"UPDATE {_tableNameLearningPaths} SET  duration = {numberCourses} WHERE  pathID = '{idPath}' ";
             var result = await connection.ExecuteAsync(sqlQuery);
